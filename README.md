9293ff435407a7d984d521bb508b364edf7a341d
Sangam Biradar <sangam14@users.noreply.github.com>
2023-03-08 15:43:56 +0530

    added-counter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fkubedaily.com%2Fk8sworkshop%2F&labelColor=%23f47373&countColor=%23263759)](https://visitorbadge.io/status?path=https%3A%2F%2Fkubedaily.com%2Fk8sworkshop%2F)
+
 - [Workshop Tracks ](https://kubedaily.com/k8sworkshop/)
--- components/footer.js
+++ components/footer.js
@@ -29,7 +29,7 @@ export default function Footer({ twitter, linkedin, github }) {
             </a>
           </li>
         ) : null}
-
+<a href="https://visitorbadge.io/status?path=https%3A%2F%2Fkubedaily.com%2Fk8sworkshop%2F"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fkubedaily.com%2Fk8sworkshop%2F&labelColor=%23f47373&countColor=%23263759" /></a>
         <li className="social">
           <div className="terms">
             <p>Code Samples and Excercises Licensed Under Apache 2.0</p>
